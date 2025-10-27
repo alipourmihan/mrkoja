e59450d7a2199757f2f5da6dbdc49fe096df22a1
ITB-Admin <admin@example.com>
2025-10-27 22:12:28 +0330

    Fix bugs in add image in businessForm4

--- mtkoja-backend/bootstrap/app.php
+++ mtkoja-backend/bootstrap/app.php
@@ -18,11 +18,11 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
         
         $middleware->web(prepend: [
-            \App\Http\Middleware\CorsMiddleware::class,
+            \Fruitcake\Cors\HandleCors::class,
         ]);
         
         $middleware->api(prepend: [
-            \App\Http\Middleware\CorsMiddleware::class,
+            \Fruitcake\Cors\HandleCors::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
